5242f792bd2b74856901b1014734c55d1a731d85
forkerino <forkerino@users.noreply.github.com>
2016-12-18 23:19:34 +0000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,165 @@
+let series=[];
+let count = 1;
+const keys = document.querySelectorAll('.key');
+let strictModeActive = false;
+let currentIndex = 0;
+const options = [{ 
+    keycode: 65, 
+    url: "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"
+  }, 
+  { keycode: 83, 
+    url: "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"
+  },
+  { keycode: 68, 
+    url: "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"
+  },
+  { keycode: 70, 
+    url: "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"
+  }];
+
+function turnOnKeys(){
+  keys.forEach(key => {
+    key.addEventListener('click',playsoundMouse);
+  })
+  window.addEventListener('keydown', playsoundKey); 
+}
+
+function turnOffKeys(){
+   keys.forEach(key => {
+    key.removeEventListener('click',playsoundMouse);
+  })
+  window.removeEventListener('keydown', playsoundKey); 
+}
+
+function startGame(){
+  setRestart();
+  
+  makeMove(count);
+  
+  turnOnKeys();  
+}
+
+function buttonBreak(interval){
+  turnOffKeys();
+  
+  setTimeout(turnOnKeys, interval);
+}
+
+function makeMove(count) {
+  displayCount(count);
+  series.push(options[Math.floor(Math.random()*4)]);
+  playSeries();
+}  
+   
+function playSeries(){
+  turnOffKeys(); 
+  console.log(series);
+  let audio = new Audio(series[0]["url"]);
+  let index=1; 
+  function playNext() {
+    if(index < series.length) {
+      audio.src = series[index]["url"];
+      audio.load(); 
+      audio.play();
+      document.querySelector(`.key[data-key="${series[index]["keycode"]}"]`).classList.add('pressed');
+      index ++;
+    } else {
+    audio.removeEventListener('ended', playNext, false);
+       turnOnKeys();
+      }
+    }
+  audio.addEventListener('ended', playNext);
+  audio.play();
+  document.querySelector(`.key[data-key="${series[0]["keycode"]}"]`).classList.add('pressed');
+
+}
+
+function restartGame(){
+  series = [];
+  currentIndex = 0;
+  count = 1;
+  setTimeout(startGame, 1500);
+}
+
+function displayCount(count){
+  let countDisplay = "0" + count;
+  document.getElementById('count').childNodes[0].innerHTML = countDisplay.slice(-2); 
+}
+
+function toggleStrictMode(){
+  let strict = document.getElementById('strict').classList;
+
+    strictModeActive = strict.toggle('strict');
+};
+
+function setRestart(){
+  let start = document.getElementById('start');
+  start.innerHTML = 'Restart';
+  start.classList.add('restart');
+  start.removeEventListener('click', startGame);
+  start.addEventListener('click', restartGame);
+}
+
+function removeTransition(e){
+  this.classList.remove('pressed');
+};
+
+function playsoundKey(e){
+  playsound(e.keyCode);
+}
+
+function playsoundMouse(e){
+  let keyClicked = this.attributes["data-key"].value;
+  playsound(keyClicked);
+};
+
+function playsound(keycode){
+  const audio = document.querySelector(`audio[data-key="${keycode}"]`);
+  const key = document.querySelector(`.key[data-key="${keycode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  
+  if (currentIndex < series.length){ // not pressed too many keys.
+    if (keycode == series[currentIndex]["keycode"]){ // right key
+      currentIndex++;
+      audio.play();
+      key.classList.add('pressed');
+      if (currentIndex === series.length) {
+        count++;
+        if (count == 21){
+          alert("You Won!");
+          restartGame();
+        } else {
+          buttonBreak(1500);
+          setTimeout(makeMove, 1500, count);
+          currentIndex = 0;
+        }
+      }
+    } else { // wrong key
+      audio.play();
+      let buttons = document.querySelectorAll('button');
+      buttons.forEach(function(button) {
+        button.classList.add('error');
+        button.addEventListener('transitionend', removeError);
+      });
+      if (strictModeActive) {
+        restartGame();
+      } else {
+        currentIndex = 0;
+        buttonBreak(1500);
+        setTimeout(playSeries, 1500);
+      }
+    }
+  } 
+}
+
+function removeError(){
+  this.classList.remove('error');
+}
+
+keys.forEach(key => {  
+  key.addEventListener('transitionend', removeTransition);
+});
+
+document.getElementById('start').addEventListener('click', startGame);
+document.getElementById('strict').addEventListener('click', toggleStrictMode);
